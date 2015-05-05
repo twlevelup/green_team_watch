c4ec7d386158f428569db30a0997f286d73f8d02
kristyvuong <kristy.vuong@gmail.com>
2015-05-05 21:08:21 +1000

    Fixed broken build

--- client/src/js/router.js
+++ client/src/js/router.js
@@ -4,10 +4,12 @@ var Router = require('./framework/router.js'),
   HomePage = require('./pages/homePage'),
   ContactsPage = require('./pages/contactsPage'),
   QuizPage = require('./pages/quizPage'),
+  InfoPage = require('./pages/infoPage'),
   ResultPage = require('./pages/resultPage'),
   homePage = new HomePage(),
   contactsPage = new ContactsPage(),
   quizPage = new QuizPage(),
+  infoPage = new InfoPage(),
   resultPage = new ResultPage();
 
 var AppRouter = Router.extend({
